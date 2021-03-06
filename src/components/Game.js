07daa19db3028b6c2e733f2a243271b125a40ea0
Solomka12 <virussoloma12>
2021-03-06 11:31:35 +0200

    Move getWinnerRow func to utils

--- src/components/Game.js
+++ src/components/Game.js
@@ -3,86 +3,12 @@ import {set} from 'lodash';
 
 import Board from './Board';
 import StatusPanel from './StatusPanel';
+
+import {getWinnerRow} from '../utils';
 import {PLAYER_SIGN} from '../constants';
 
 const boardSize = 10;
 const marksToWin = 5;
-
-const fitsVertically = i => i + boardSize * (marksToWin - 1) < boardSize * boardSize;
-const fitsHorizontally = i => i % boardSize + marksToWin <= boardSize;
-const fitsBackHorizontally = i => i % boardSize - (marksToWin - 1) >= 0;
-
-const getHorizontalRow = (i, cell, board = []) => {
-    if (!fitsHorizontally(i)) return null;
-
-    let currWinnerRow = [i];
-    let currRowCount = 1;
-
-    for (let j = i + 1; currRowCount < marksToWin; j++) {
-        if (board[j] === cell) {
-            currWinnerRow.push(j);
-            currRowCount++;
-        } else {
-            break;
-        }
-    }
-
-    return currWinnerRow.length >= marksToWin ? currWinnerRow : null;
-};
-
-const getVerticalRow = (i, cell, board = []) => {
-    if (!fitsVertically(i)) return null;
-
-    let currWinnerRow = [i];
-    let currRowCount = 1;
-
-    for (let j = i + boardSize; currRowCount < marksToWin; j += boardSize) {
-        if (board[j] === cell) {
-            currWinnerRow.push(j);
-            currRowCount++;
-        } else {
-            break;
-        }
-    }
-
-    return currWinnerRow.length >= marksToWin ? currWinnerRow : null;
-};
-
-const getDiagonalLTRRow = (i, cell, board = []) => {
-    if (!fitsHorizontally(i) || !fitsVertically(i)) return null;
-
-    let currWinnerRow = [i];
-    let currRowCount = 1;
-
-    for (let j = i + boardSize + 1; currRowCount < marksToWin; j += boardSize + 1) {
-        if (board[j] === cell) {
-            currWinnerRow.push(j);
-            currRowCount++;
-        } else {
-            break;
-        }
-    }
-
-    return currWinnerRow.length >= marksToWin ? currWinnerRow : null;
-};
-
-const getDiagonalRTLRow = (i, cell, board = []) => {
-    if (!fitsVertically(i) || !fitsBackHorizontally(i)) return null;
-
-    let currWinnerRow = [i];
-    let currRowCount = 1;
-
-    for (let j = i + boardSize - 1; currRowCount < marksToWin; j += boardSize - 1) {
-        if (board[j] === cell) {
-            currWinnerRow.push(j);
-            currRowCount++;
-        } else {
-            break;
-        }
-    }
-
-    return currWinnerRow.length >= marksToWin ? currWinnerRow : null;
-};
 
 const initialScore = {[PLAYER_SIGN.X]: 0, [PLAYER_SIGN.O]: 0}
 
@@ -98,7 +24,7 @@ export default function Game() {
     }, []);
 
     useEffect(() => {
-        const row = getWinnerRow();
+        const row = getWinnerRow(board, boardSize, marksToWin);
         if (row) setWinnerRow(row);
     }, [board]);
 
@@ -128,26 +54,6 @@ export default function Game() {
 
     const togglePlayer = () => {
         setCurrentPlayer(p => p === PLAYER_SIGN.X ? PLAYER_SIGN.O : PLAYER_SIGN.X);
-    };
-
-    const getWinnerRow = () => {
-        for (let i = 0; i < board.length; i++) {
-            const cell = board[i];
-
-            if (cell) {
-                const horizontalRow = getHorizontalRow(i, cell, board);
-                if (horizontalRow) return horizontalRow;
-
-                const verticalRow = getVerticalRow(i, cell, board);
-                if (verticalRow) return verticalRow;
-
-                const diagonalLTRRow = getDiagonalLTRRow(i, cell, board);
-                if (diagonalLTRRow) return diagonalLTRRow;
-
-                const diagonalRTLRow = getDiagonalRTLRow(i, cell, board);
-                if (diagonalRTLRow) return diagonalRTLRow;
-            }
-        }
     };
 
     return (
--- src/utils.js
+++ src/utils.js
@@ -3,3 +3,99 @@ export const getSplitArr = (arr, gridCount = 10) => {
         .fill(null)
         .map((item, i) => arr.slice(gridCount * i, gridCount * i + gridCount));
 }
+
+export const getWinnerRow = (board, boardSize, marksToWin) => {
+    const fitsVertically = i => i + boardSize * (marksToWin - 1) < boardSize * boardSize;
+    const fitsHorizontally = i => i % boardSize + marksToWin <= boardSize;
+    const fitsBackHorizontally = i => i % boardSize - (marksToWin - 1) >= 0;
+
+    const getHorizontalRow = (i, cell, board = []) => {
+        if (!fitsHorizontally(i)) return null;
+
+        let currWinnerRow = [i];
+        let currRowCount = 1;
+
+        for (let j = i + 1; currRowCount < marksToWin; j++) {
+            if (board[j] === cell) {
+                currWinnerRow.push(j);
+                currRowCount++;
+            } else {
+                break;
+            }
+        }
+
+        return currWinnerRow.length >= marksToWin ? currWinnerRow : null;
+    };
+
+    const getVerticalRow = (i, cell, board = []) => {
+        if (!fitsVertically(i)) return null;
+
+        let currWinnerRow = [i];
+        let currRowCount = 1;
+
+        for (let j = i + boardSize; currRowCount < marksToWin; j += boardSize) {
+            if (board[j] === cell) {
+                currWinnerRow.push(j);
+                currRowCount++;
+            } else {
+                break;
+            }
+        }
+
+        return currWinnerRow.length >= marksToWin ? currWinnerRow : null;
+    };
+
+    const getDiagonalLTRRow = (i, cell, board = []) => {
+        if (!fitsHorizontally(i) || !fitsVertically(i)) return null;
+
+        let currWinnerRow = [i];
+        let currRowCount = 1;
+
+        for (let j = i + boardSize + 1; currRowCount < marksToWin; j += boardSize + 1) {
+            if (board[j] === cell) {
+                currWinnerRow.push(j);
+                currRowCount++;
+            } else {
+                break;
+            }
+        }
+
+        return currWinnerRow.length >= marksToWin ? currWinnerRow : null;
+    };
+
+    const getDiagonalRTLRow = (i, cell, board = []) => {
+        if (!fitsVertically(i) || !fitsBackHorizontally(i)) return null;
+
+        let currWinnerRow = [i];
+        let currRowCount = 1;
+
+        for (let j = i + boardSize - 1; currRowCount < marksToWin; j += boardSize - 1) {
+            if (board[j] === cell) {
+                currWinnerRow.push(j);
+                currRowCount++;
+            } else {
+                break;
+            }
+        }
+
+        return currWinnerRow.length >= marksToWin ? currWinnerRow : null;
+    };
+
+    for (let i = 0; i < board.length; i++) {
+        const cell = board[i];
+
+        if (cell) {
+            const horizontalRow = getHorizontalRow(i, cell, board);
+            if (horizontalRow) return horizontalRow;
+
+            const verticalRow = getVerticalRow(i, cell, board);
+            if (verticalRow) return verticalRow;
+
+            const diagonalLTRRow = getDiagonalLTRRow(i, cell, board);
+            if (diagonalLTRRow) return diagonalLTRRow;
+
+            const diagonalRTLRow = getDiagonalRTLRow(i, cell, board);
+            if (diagonalRTLRow) return diagonalRTLRow;
+        }
+    }
+};
